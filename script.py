abc814f2ded87a82e97f6737f45f58599303568b
Jair Morocho <jairmorocho99@gmail.com>
2020-12-15 16:55:54 -0500

    Agregando datos del proyecto

--- script.py
+++ script.py
@@ -1,10 +1,11 @@
 # Script de prueba para Git
-# Autor: Rodrigo Tufiño <rtufino@ups.edu.ec>
-# Fecha: 23-06-2020
+# Autor: Jair Morocho <jairmorocho99@gmail.com>
+# Fecha: 15-12-2020
 # Versión: 1.0.0
 
 def main():
-    print("Hola Mundo!")
+    nombre = input("Cual es tu nombre?")
+    print("Hola ",nombre," Bienvenido a Git!")
 
 if __name__ == "__main__":
     main()
