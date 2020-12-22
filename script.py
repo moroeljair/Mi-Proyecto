8140e2ba06d7e38b81ecdd028d6a18491dcee9e7
Josue Morales <josue_morales_riv11@hotmail.com>
2020-12-22 00:57:17 -1200

    Cambiando, ando

--- script.py
+++ script.py
@@ -7,5 +7,7 @@ def main():
     nombre = input("Cual es tu nombre?")
     print("Hola ",nombre," Bienvenido a Git!")
     print("Hola, que mas pues"); #by Josue Morales
+    print("Segunda linea de comando"); #by Josue Morales
 if __name__ == "__main__":
     main()
+
